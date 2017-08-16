8fd2ce2ee82d4b73a9ca7a2d6942a0920b949da3
BlaiseGratton <blaisegratton@gmail.com>
2017-08-16 10:41:17 -0500

    debounce piece move stream and fix display of pieces

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+
+import 'rxjs/add/operator/debounceTime';
 import { Subscription } from 'rxjs/Subscription';
 
 import { Isquare } from '../../shared/isquare';
@@ -35,22 +37,207 @@ export class BoardComponent implements OnInit {
 
   private setupBoard = () => {
     this.board[0][0].piece = {
-      name: 'Rook',
+      name: 'r1',
       player: null,
-      checkMoves: (loc: Isquare) : Isquare[] => {
-        return null
-      }
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[0][1].piece = {
+      name: 'k1',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[0][2].piece = {
+      name: 'b1',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[0][3].piece = {
+      name: 'q',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[0][4].piece = {
+      name: 'k',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[0][5].piece = {
+      name: 'b2',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[0][6].piece = {
+      name: 'k2',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[0][7].piece = {
+      name: 'r2',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[1][0].piece = {
+      name: 'p1',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[1][1].piece = {
+      name: 'p2',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[1][2].piece = {
+      name: 'p3',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[1][3].piece = {
+      name: 'p4',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[1][4].piece = {
+      name: 'p5',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[1][5].piece = {
+      name: 'p6',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[1][6].piece = {
+      name: 'p7',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[1][7].piece = {
+      name: 'p8',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[7][0].piece = {
+      name: 'r1',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[7][1].piece = {
+      name: 'k1',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[7][2].piece = {
+      name: 'b1',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[7][3].piece = {
+      name: 'q',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[7][4].piece = {
+      name: 'k',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[7][5].piece = {
+      name: 'b2',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[7][6].piece = {
+      name: 'k2',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[7][7].piece = {
+      name: 'r2',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[6][0].piece = {
+      name: 'p1',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[6][1].piece = {
+      name: 'p2',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[6][2].piece = {
+      name: 'p3',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[6][3].piece = {
+      name: 'p4',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[6][4].piece = {
+      name: 'p5',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[6][5].piece = {
+      name: 'p6',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[6][6].piece = {
+      name: 'p7',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
+    }
+
+    this.board[6][7].piece = {
+      name: 'p8',
+      player: null,
+      checkMoves: (loc: Isquare) : Isquare[] => null
     }
   }
 
   private processMove(message: any) {
-    debugger
+    console.log('process move')
   }
 
   ngOnInit() {
     this.generateBoard()
     this.setupBoard()
     this.movesSubscription = this.moveService.move$
+      .debounceTime(10)
       .subscribe(move => this.processMove(move))
   }
 
--- src/app/components/piece/piece.component.ts
+++ src/app/components/piece/piece.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import 'rxjs/add/operator/debounceTime';
 import { DragulaService } from 'ng2-dragula/ng2-dragula'
 
 import { Ipiece } from '../../shared/ipiece'
@@ -20,25 +21,24 @@ export class PieceComponent implements OnInit {
     private moveService: MoveService
   ) { }
 
-  dragHandler(that: PieceComponent): Function {
-    return function (val: any): void {
-      const squareId = val[2].getAttribute('item-id')
-      const pieceId = val[1].getAttribute('item-id')
-      that.moveService.registerDrag({ squareId, pieceId })
-    }
+  dragHandler(val: any): void {
+    const squareId = val[2].getAttribute('item-id')
+    const pieceId = val[1].getAttribute('item-id')
+    this.moveService.registerDrag({ squareId, pieceId })
   }
 
-  dropHandler(that: PieceComponent): Function {
-    return function (val: any): void {
-      const cancelFunc = that.dragula.find('drop-square').drake.cancel
-      const squareId = val[2].getAttribute('item-id')
-      const pieceId = val[1].getAttribute('item-id')
-      that.moveService.registerDrop({ squareId, pieceId, cancelFunc })
-    }
+  dropHandler(val: any): void {
+    const cancelFunc = this.dragula.find('drop-square').drake.cancel
+    const squareId = val[2].getAttribute('item-id')
+    const pieceId = val[1].getAttribute('item-id')
+    this.moveService.registerDrop({ squareId, pieceId, cancelFunc })
   }
 
   ngOnInit() {
-    this.dragula.drag.subscribe(this.dragHandler(this))
-    this.dragula.drop.subscribe(this.dropHandler(this))
+    this.dragula.drag
+      .subscribe(val => this.dragHandler(val))
+
+    this.dragula.drop
+      .subscribe(val => this.dropHandler(val))
   }
 }
